baebab83609b7403a178acb1279d133e6d916468
REPIDUS <moondj123@naver.com>
2019-05-30 16:43:58 +0900

    Create header, nav, article as components
    기능: header, nav, article 생성 및 컴포넌트화, components 디렉터리로 분리
    디자인: 배경색 제거

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Subject from './components/Subject';
+import TOC from './components/TOC';
+import Content from './components/Content';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React.js!!
+        <Subject title="WEB" sub="World Wide Web!"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
