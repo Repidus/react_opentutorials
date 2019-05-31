02936ef9841be09d0e0256e9a7ef42eb7430fb98
REPIDUS <moondj123@naver.com>
2019-05-31 20:07:07 +0900

    Change data source of Subject & TOC components to state of App
    리팩토링: Subject와 TOC 컴포넌트가 props로 전달받던 데이터를 App 컴포넌트의 state에서 전달받도록 변경

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
   render() {
-    return(
+    var lists = [];
+    var data = this.props.data;
+    for (var i = 0; i < data.length; i++) {
+      lists.push(<li id={data[i].id}><a href={'/content/'+data[i].id}>{data[i].title}</a></li>)
+    }
+    return (
       <nav>
         <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
+          {lists}
         </ul>
       </nav>
     );
