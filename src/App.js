2d0a50d745b7925749d7752a30d4b131e5a00b8b
REPIDUS <moondj123@naver.com>
2019-10-11 20:42:26 +0900

    Make Content differ by mode of App
    기능: App의 mode에 따라 Content가 웰컴 페이지 혹은 항목 내용을 표시하게 함.
    기능: Subject의 title 링크가 App의 mode를 welcome으로 바꾸게 함.
    기능: TOC의 리스트 항목 링크가 App의 mode를 read로 바꾸고 Content의 내용을 바꾸게 함.
    오타: TOC가 반환하는 리스트의 각 항목에 대한 key 어트리뷰트 이름을 id에서 key로 고침.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'welcome',
+      selected_content_id: 0,
+      welcome: {title: 'Welcome', desc: "Hello, React!"},
       subject: {title: "WEB", sub: "World Wide Web!"},
       contents: [
         {id: 1, title: 'HTML', desc: 'HTML is for information'},
@@ -17,14 +20,40 @@ class App extends Component {
     };
   }
   render() {
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      for (var i = 0; i < this.state.contents.length; i++) {
+        if (this.state.contents[i].id === this.state.selected_content_id) {
+          _title = this.state.contents[i].title;
+          _desc = this.state.contents[i].desc;
+          break;
+        }
+      }
+    }
     return (
       <div className="App">
         <Subject 
           title={this.state.subject.title} 
-          sub={this.state.subject.sub}>
+          sub={this.state.subject.sub}
+          onChangePage={function() {
+            this.setState({
+              mode: 'welcome'
+            });
+          }.bind(this)}>
         </Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <TOC 
+          data={this.state.contents}
+          onChangePage={function(id) {
+            this.setState({
+              mode: 'read',
+              selected_content_id: Number(id)
+            });
+          }.bind(this)}>
+        </TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -5,7 +5,18 @@ class TOC extends Component {
     var lists = [];
     var data = this.props.data;
     for (var i = 0; i < data.length; i++) {
-      lists.push(<li id={data[i].id}><a href={'/content/'+data[i].id}>{data[i].title}</a></li>)
+      lists.push(
+        <li key={data[i].id}>
+          <a 
+            href={'/content/'+data[i].id}
+            onClick={function(id, e) {
+              e.preventDefault();
+              this.props.onChangePage(id);
+            }.bind(this, data[i].id)}>
+            {data[i].title}
+          </a>
+        </li>
+      );
     }
     return (
       <nav>
